1ce9aa96d16c492250a68e17c3ab639b2f399e3f
Takashi Kitao <takashi.kitao@gmail.com>
2015-03-29 11:51:18 +0900

    Changed the way to detect boot phase

--- lib/processing.rb
+++ lib/processing.rb
@@ -1,8 +1,8 @@
-if RUBY_PLATFORM != 'java'
+if Object.const_defined? :SketchRunner
+  require_relative 'processing/system'
+  require_relative 'processing/sketch_base'
+else
   require_relative 'sketch_runner/config'
   require_relative 'sketch_runner/setup'
   require_relative 'sketch_runner/launch'
-else
-  require_relative 'processing/system'
-  require_relative 'processing/sketch_base'
 end
